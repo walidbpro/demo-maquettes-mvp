9011c2c299aabc2fa85e191ee044ab624acca8d9
Walidm <oblila.ext@freelance.com>
2026-07-22 23:48:58 +0200

    Handle Metabase guest embed preflight

--- api/metabase-guest-token.js
+++ api/metabase-guest-token.js
@@ -31,8 +31,16 @@ function signGuestToken(secret) {
 }
 
 module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   if (req.method !== "GET" && req.method !== "POST") {
-    res.setHeader("Allow", "GET, POST");
+    res.setHeader("Allow", "GET, POST, OPTIONS");
     return res.status(405).json({ error: "Method not allowed" });
   }
 
